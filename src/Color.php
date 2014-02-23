4c65a95b7524eae02389a84fbbce4dc2350434c2
AJ Ostrow <alexander.ostrow@gmail.com>
2014-02-23 11:33:39 -0500

    initial commit

---
+++ src/Color.php
@@ -0,0 +1,5 @@
+<?php
+namespace mcgillenergy\color;
+
+class Color {
+}
---
+++ test/ColorTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace mcgillenergy\color;
+
+class ColorTest extends \PHPUnit_Framework_TestCase
+{
+  public function testConstructor()
+  {
+    $color = new Color();
+  }
+}
