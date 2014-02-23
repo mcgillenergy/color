c5425ebe6cf14db255f9241a487abb3869575a9d
AJ Ostrow <alexander.ostrow@gmail.com>
2014-02-23 12:24:59 -0500

    rgb

--- src/Color.php
+++ src/Color.php
@@ -1,5 +1,39 @@
 <?php
 namespace mcgillenergy\color;
 
-class Color {
+class Color
+{
+  private $red, $green, $blue;
+
+  // rgb: 0..255
+  public function __construct($rgb)
+  {    
+    $this->red = $rgb[0];
+    $this->green = $rgb[1];
+    $this->blue = $rgb[2];
+  }
+
+  // $white = Color::RGB(0xffffff)
+  // $white = COlor::RGB(255, 255, 255)
+  public static function RGB(/* args */)
+  {
+    $argc = func_num_args();
+    if ($argc == 3) {
+      return new Color(func_get_args());
+    } elseif ($argc == 1) {
+      $hex = func_get_arg(0);
+      $rgb = array();
+      while ($hex > 0)
+      {
+        array_push($rgb, $hex & 0xff);
+        $hex >>= 8;
+      }
+      return new Color($rgb);
+    }
+  }
+
+  public function toRGB()
+  {
+    return array('r' => $this->red, 'g' => $this->green, 'b' => $this->blue);
+  }
 }
--- test/ColorTest.php
+++ test/ColorTest.php
@@ -5,6 +5,20 @@ class ColorTest extends \PHPUnit_Framework_TestCase
 {
   public function testConstructor()
   {
-    $color = new Color();
+    $this->assertInstanceOf('mcgillenergy\\color\\Color', new Color(0, 0, 0));
+  }
+
+  public function testRGB()
+  {
+    $white = Color::RGB(255, 255, 255);
+    $rgb = array('r' => 255, 'g' => 255, 'b' => 255);
+    $this->assertEquals($white->toRGB(), $rgb);
+  }
+
+  public function testHexRGB()
+  {
+    $white = Color::RGB(0xffffff);
+    $rgb = array('r' => 255, 'g' => 255, 'b' => 255);
+    $this->assertEquals($white->toRGB(), $rgb);
   }
 }
