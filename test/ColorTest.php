7f6d7541aefa96eb26bb044b1b472e48c56153de
AJ Ostrow <alexander.ostrow@gmail.com>
2014-02-23 12:50:35 -0500

    rgba supported

--- test/ColorTest.php
+++ test/ColorTest.php
@@ -21,4 +21,32 @@ class ColorTest extends \PHPUnit_Framework_TestCase
     $rgb = array('r' => 255, 'g' => 255, 'b' => 255);
     $this->assertEquals($white->toRGB(), $rgb);
   }
+
+  public function testRGBtoRGBA()
+  {
+    $white = Color::RGB(0xffffff);
+    $rgba = array('r' => 255, 'g' => 255, 'b' => 255, 'a' => 1.0);
+    $this->assertEquals($white->toRGBA(), $rgba);
+  }
+
+  public function testRGBA()
+  {
+    $white = Color::RGBA(255, 255, 255, 1.0);
+    $rgba = array('r' => 255, 'g' => 255, 'b' => 255, 'a' => 1.0);
+    $this->assertEquals($white->toRGBA(), $rgba);
+  }
+
+  public function testHexRGBA()
+  {
+    $white = Color::RGBA(0xffffff, 1.0);
+    $rgba = array('r' => 255, 'g' => 255, 'b' => 255, 'a' => 1.0);
+    $this->assertEquals($white->toRGBA(), $rgba);
+  }
+
+  public function testRGBAtoRGB()
+  {
+    $white = Color::RGBA(0xffffff, 1.0);
+    $rgb = array('r' => 255, 'g' => 255, 'b' => 255);
+    $this->assertEquals($white->toRGB(), $rgb);
+  }
 }
